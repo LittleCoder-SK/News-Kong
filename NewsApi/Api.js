1425742839c723f3ffbffcd45736191c7c319cc4
LittleCoder-SK <soluk224@gmail.com>
2026-01-19 12:29:31 +0530

    infinitescroll

--- NewsApi/Api.js
+++ NewsApi/Api.js
@@ -47,7 +47,7 @@ const FetchData = async (page, currentCategory) => {
 
 const Render = (articles) => {
 
-  innercard.innerHTML = ""
+  // innercard.innerHTML = ""
 
   articles.forEach(val => {
 
@@ -76,33 +76,48 @@ navlist.forEach(function (item) {
     item.classList.add("bg-indigo-500", "text-white", "scale-105");
 
     //when you navigate using category every time news fetch on currentPage 1
-    currentPage = 1
+    currentPage = 1;
+    totalPages = 1;
+    innercard.innerHTML = "";
 
     currentCategory = item.innerText;
-    console.log(currentCategory)
     FetchData(currentPage, currentCategory);
 
   });
 
 })
 
+const infinitScroll = () =>{
+ window.addEventListener("scroll", () => {
+  const scrollPos = window.innerHeight + window.scrollY;
+  const threshold = document.body.offsetHeight * 0.8;
+
+  if (scrollPos >= threshold && !isLoading && currentPage < totalPages) {
+    currentPage++;
+    FetchData(currentPage, currentCategory);
+  }
+});
+}
+
+infinitScroll()
+
 
 // Go bact to the previous page
-const Prevpage = () => {
+// const Prevpage = () => {
 
-  if (currentPage <= 1) return;
-  currentPage--
-  FetchData(currentPage, currentCategory);
-}
+//   if (currentPage <= 1) return;
+//   currentPage--
+//   FetchData(currentPage, currentCategory);
+// }
 
 // Go to the next page
-const Nextpage = () => {
+// const Nextpage = () => {
 
-  if (currentPage >= totalPages) return;
-  currentPage++
-  FetchData(currentPage, currentCategory);
+//   if (currentPage >= totalPages) return;
+//   currentPage++
+//   FetchData(currentPage, currentCategory);
 
-}
+// }
 
 // this is for disabling or bg-color change to button if below the condition are satisfied
 function updateControls() {
@@ -115,6 +130,27 @@ function updateControls() {
   next.disabled = currentPage === totalPages;
   if (currentPage === totalPages ? next.style.background = "darkgray" : next.style.background = "black");
 }
+
+const backToTop = document.getElementById("backToTop");
+
+// Show button when scrolling down
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 300) {
+    backToTop.classList.remove("hidden");
+    backToTop.classList.add("opacity-100");
+  } else {
+    backToTop.classList.add("hidden");
+  }
+});
+
+// Smooth scroll to top
+backToTop.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
+
 
 
 
